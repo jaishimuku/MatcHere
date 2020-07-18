65483375d0a0bee4451c82ada34a338e7487ef15
Mukunda P Jaishi <mukundajaishi@Mukundas-MacBook-Pro.local>
2020-07-18 18:09:53 -0500

    fixed minor error --> appsettings.json

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -30,7 +30,7 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DataContext")));
             services.AddControllers();
             services.AddCors();
             services.AddScoped<IAuthRepository, AuthRepository>(); // service is created once per request; one instance per HTTP request and used in another request
