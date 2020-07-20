20c6b8f0811987c22ee6a654e30470e385da9bde
Mukunda P Jaishi <mukundajaishi@Mukundas-MacBook-Pro.local>
2020-07-20 16:36:47 -0500

    used alertifyjs for notification, angular jwt to decode token, for theme used ngx bootstrap and bootswatch

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,25 +11,24 @@ export class NavComponent implements OnInit {
 
   model: any = {}; // created object to store values i.e. username and password
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
   login(){
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in successfully');
+      this.alertify.success('Logged in successfully');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   loggedIn() { // if logged in shows the User Message
-    const token = localStorage.getItem('token');
-    return !!token; // if token is empty returns false otherwise if something then true
+    return this.authService.loggedIn();
   }
 
   logout() { // removes token form local storage
     localStorage.removeItem('token');
-    console.log('Logged Out');
+    this.alertify.message('Logged Out');
   }
 }
