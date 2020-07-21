77910450af75731c85dee35459fd19895ea8b66e
jaishimuku <mukunda.jaishi@selu.edu>
2020-07-21 17:26:33 -0500

    Routing in client, Routes Protected(authentication), used RouterLinkActive

---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    {
+        path: '', //localhost:4200/
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent},
+            { path: 'messages', component: MemberListComponent},
+            { path: 'lists', component: ListsComponent},
+        ]
+    },
+   { path: '**', redirectTo: '', pathMatch: 'full'}, // this should be at last  "order matters here"
+];
