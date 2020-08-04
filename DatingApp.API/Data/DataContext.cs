db94a467921af1eeb7fb60e7979dfa5ee0a6ecf8
jaishimuku <mukunda.jaishi@selu.edu>
2020-08-04 17:01:36 -0500

    API added for creatimg messages

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -16,6 +16,8 @@ namespace DatingApp.API.Data
 
         public DbSet <Like> Likes { get; set; }
 
+        public DbSet <Message> Messages { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder) 
         {
             builder.Entity<Like>()
@@ -31,6 +33,16 @@ namespace DatingApp.API.Data
                 .WithMany( u => u.Likees)
                 .HasForeignKey( u => u.LikerId)
                 .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(u => u.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+            .HasOne(u => u.Recipient)
+            .WithMany( u => u.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
